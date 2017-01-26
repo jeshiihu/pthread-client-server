0d06ab690498aaf05eccae4e3c71bf2832c02713
Jessica Huynh <jhuynh@ualberta.ca>
2017-01-26 15:58:22 -0700

    some mods to client server, for args and inits

--- client.c
+++ client.c
@@ -6,28 +6,37 @@
 #include<arpa/inet.h>
 #include<unistd.h>
 
-int main()
+int NUM_STR;
+
+int main(int argc, char* argv[])
 {
+	if(argc != 3) {
+		printf("Error: incorrect number of arguments, please try again\n");
+		return 0;
+	}
+
+	NUM_STR = atoi(argv[2]); // number of string in array
+
 	struct sockaddr_in sock_var;
 	int clientFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
 	char str_clnt[20],str_ser[20];
 
 	sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
-	sock_var.sin_port=3000;
+	sock_var.sin_port=atoi(argv[1]); // pass in port number by command line arg
 	sock_var.sin_family=AF_INET;
 
 	if(connect(clientFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
 	{
-		printf("Connected to server %dn",clientFileDescriptor);
-		printf("nEnter Srting to send");
+		printf("Connected to server %d\n",clientFileDescriptor);
+		printf("\nEnter String to send: ");
 		scanf("%s",str_clnt);
 		write(clientFileDescriptor,str_clnt,20);
 		read(clientFileDescriptor,str_ser,20);
-		printf("String from Server: %s",str_ser);
+		printf("String from Server: %s\n",str_ser);
 		close(clientFileDescriptor);
 	}
 	else{
-		printf("socket creation failed");
+		printf("\nsocket creation failed\n");
 	}
 	return 0;
 }
--- server.c
+++ server.c
@@ -7,21 +7,31 @@
 #include<unistd.h>
 #include<pthread.h>
 
-void *ServerEcho(void *args)
+#define thread_count 1000
+#define STR_LEN 1000
+
+int NUM_STR; 
+char** theArray;
+unsigned int* seed;
+pthread_mutex_t mutex;
+
+void initSeed();
+void initArray();
+void *ServerEcho(void *args);
+void *Operate(void* rank);  /* Thread function */
+
+// command line arguements 
+int main(int argc, char* argv[])
 {
-	int clientFileDescriptor=(int)args;
-	char str[20];
+	if(argc != 3) {
+		printf("Error: incorrect number of arguments, please try again\n");
+		return 0;
+	}
 
-	read(clientFileDescriptor,str,20);
-	printf("nreading from client:%s",str);
-	write(clientFileDescriptor,str,20);
-	printf("nechoing back to client");
-	close(clientFileDescriptor);
-}
+	NUM_STR = atoi(argv[2]); // number of strings in the array
+	initSeed();
+	initArray();
 
-
-int main()
-{
 	struct sockaddr_in sock_var;
 	int serverFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
 	int clientFileDescriptor;
@@ -29,25 +39,77 @@ int main()
 	pthread_t t[20];
 
 	sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
-	sock_var.sin_port=3000;
+	sock_var.sin_port=atoi(argv[1]);
 	sock_var.sin_family=AF_INET;
 	if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
 	{
-		printf("nsocket has been created");
+		printf("\nsocket has been created");
 		listen(serverFileDescriptor,2000); 
 		while(1)        //loop infinity
 		{
 			for(i=0;i<20;i++)      //can support 20 clients at a time
 			{
 				clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
-				printf("nConnected to client %dn",clientFileDescriptor);
-				pthread_create(&t,NULL,ServerEcho,(void *)clientFileDescriptor);
+				printf("\nConnected to client %d\n",clientFileDescriptor);
+				pthread_create(&t,NULL,ServerEcho,(void *)(uintptr_t)clientFileDescriptor);
 			}
 		}
 		close(serverFileDescriptor);
 	}
 	else{
-		printf("nsocket creation failed");
+		printf("\nsocket creation failed\n");
 	}
 	return 0;
+}
+
+void *ServerEcho(void *args)
+{
+	int clientFileDescriptor=(int)args;
+	char str[20];
+
+	read(clientFileDescriptor,str,20);
+	printf("\nreading from client: %s",str);
+	write(clientFileDescriptor,str,20);
+	printf("\nechoing back to client");
+	close(clientFileDescriptor);
+
+	return NULL;
+}
+
+void initSeed() {
+	seed = malloc(thread_count*sizeof(int));
+
+	int i;
+	for (i = 0; i < thread_count; i++)
+		seed[i] = i;
+}
+
+void initArray() {
+	// initialize the array with number of strings
+	theArray = malloc(NUM_STR*sizeof(char[STR_LEN]));
+
+	int i;
+	for(i = 0; i < NUM_STR; i++) {
+		theArray[i] = malloc(STR_LEN*sizeof(char));
+	}
+}
+
+void *Operate(void* rank) {
+	long my_rank = (long) rank;
+	
+	// Find a random position in theArray for read or write
+	int pos = rand_r(&seed[my_rank]) % NUM_STR;
+	int randNum = rand_r(&seed[my_rank]) % 100;	// write with 5% probability
+	
+	pthread_mutex_lock(&mutex); 
+
+	if (randNum >= 95) // 95% are write operations, others are reads
+		sprintf(theArray[pos], "theArray[%d] modified by thread %ld", pos, my_rank);
+
+	printf("Thread %ld: randNum = %d\n", my_rank, randNum);
+	printf("%s\n\n", theArray[pos]); // return the value read or written
+
+	pthread_mutex_unlock(&mutex);
+		
+	return NULL;
 }
